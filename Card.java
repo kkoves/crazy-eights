ffa3b833056fab40ab46b151fd0e79d0def8f892
unknown <brendon_boldt@yahoo.com>
2014-09-22 14:34:50 -0400

    Enumerated types for rank and suit added

---
+++ Card.java
@@ -0,0 +1,33 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Card implements ICard {
+  Rank rank;
+  Suit suit;
+  
+
+  
+  public static void main(String[] args) { 
+    System.out.println(new Card(Card.Rank.Ace,Card.Suit.Spades));
+    System.out.println(new Card(Card.Rank.Jack,Card.Suit.Diamonds));
+    System.out.println(new Card(Card.Rank.Two,Card.Suit.Clubs));
+  }
+  
+  public Card(Rank rank, Suit suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+  
+  public Rank getRank() {
+    return this.rank;
+  }
+  
+  public Suit getSuit() {
+    return this.suit;
+  }
+       
+  @Override
+  public String toString() {
+    return rank + " of " + suit;
+  } 
+}
---
+++ ICard.java
@@ -0,0 +1,21 @@
+/**
+ * Card interface
+ * @authors Krisztián Köves, Brendon Boldt
+ * @date 9/15/2014
+ */
+
+public interface ICard {
+  public Rank getRank();
+  public Suit getSuit();
+  
+  public enum Rank {
+    Ace, One, Two, Three,
+    Four, Five, Six, Seven,
+    Eight, Nine, Ten,
+    Jack, Queen, King;
+  }
+  
+  public enum Suit {
+    Clubs, Hearts, Spades, Diamonds;
+  }
+}
